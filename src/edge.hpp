e413d423407622e4aa1fb789ce93a266838cf795
ko-cha <xtej3g@gmail.com>
2020-02-19 17:54:18 +0900

    :pencil: add some operator

--- src/edge.hpp
+++ src/edge.hpp
@@ -3,5 +3,18 @@
 #include "node.hpp"
 
 struct Edge {
+
+    // member variable
     Node u, v;
+
+    // operator
+    bool operator == (const Edge & e) const;
+    bool operator != (const Edge & e) const;
 };
+
+bool Edge::operator == (const Edge & e) const {
+    return (u == e.u && v == e.v) || (u == e.v && v == e.u);
+}
+bool Edge::operator != (const Edge & e) const {
+    return !((u == e.u && v == e.v) || (u == e.v && v == e.u));
+}
