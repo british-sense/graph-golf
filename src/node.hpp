692c6a9111450c519cd8b485da663fd3cc3742f6
ko-cha <xtej3g@gmail.com>
2020-02-19 18:15:11 +0900

    :pencil: implemented constracotr

--- src/node.hpp
+++ src/node.hpp
@@ -3,14 +3,23 @@
 #include "point.hpp"
 
 struct Node : Point{
-    
+
     // member variable
     int id;
+
+    // constractor
+    Node();
+    Node(int id);
+    Node(int h, int w);
 
     // operator
     bool operator == (const Node & n) const;
     bool operator != (const Node & n) const;
 };
+
+Node::Node() {}
+Node::Node(int _id) : id(_id) {}
+Node::Node(int _h, int _w) : Point(_h, _w) {}
 
 bool Node::operator == (const Node & n) const {
     return id == n.id;
