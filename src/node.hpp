d8c076f37c8e7206a7afab5aff0d2fc2823f72f0
ko-cha <xtej3g@gmail.com>
2020-02-19 17:52:10 +0900

    :pencil: add some operator

--- src/node.hpp
+++ src/node.hpp
@@ -3,5 +3,18 @@
 #include "point.hpp"
 
 struct Node : Point{
+    
+    // member variable
     int id;
+
+    // operator
+    bool operator == (const Node & n) const;
+    bool operator != (const Node & n) const;
 };
+
+bool Node::operator == (const Node & n) const {
+    return id == n.id;
+}
+bool Node::operator != (const Node & n) const {
+    return id != n.id;
+}
