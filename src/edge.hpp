2b2bcc7ce33ac8aa31961e58d0cc4c18c074b5aa
ko-cha <xtej3g@gmail.com>
2020-02-19 18:26:51 +0900

    ;pencil: implemented some constractor

--- src/edge.hpp
+++ src/edge.hpp
@@ -7,10 +7,17 @@ struct Edge {
     // member variable
     Node u, v;
 
+    // constractor
+    Edge();
+    Edge(Node u, Node v);
+
     // operator
     bool operator == (const Edge & e) const;
     bool operator != (const Edge & e) const;
 };
+
+Edge::Edge() {}
+Edge::Edge(Node _u, Node _v) : u(_u), v(_v) {}
 
 bool Edge::operator == (const Edge & e) const {
     return (u == e.u && v == e.v) || (u == e.v && v == e.u);
