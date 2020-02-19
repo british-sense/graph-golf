89038355bfddc5908c48eedf0a7b16943cc74bef
ko-cha <xtej3g@gmail.com>
2020-02-19 17:50:29 +0900

    :pencil: add static parameter

--- src/params.hpp
+++ src/params.hpp
@@ -1,4 +1,16 @@
 #pragma once
 #include <random>
 
+// general
+static int degree;
+
+// general graph
+static int node;
+
+// grid graph
+static int height;
+static int width;
+static int length;
+
+// others
 static std::mt19937 mt;
