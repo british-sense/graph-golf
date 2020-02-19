48c645128553ccda94ce33c237941f41f35722bf
ko-cha <xtej3g@gmail.com>
2020-02-19 22:23:11 +0900

    :pencil: implemented edge fucntion

--- src/edge.hpp
+++ src/edge.hpp
@@ -11,6 +11,9 @@ struct Edge {
     Edge();
     Edge(Node u, Node v);
 
+    // member function
+    std::string output();
+
     // operator
     bool operator == (const Edge & e) const;
     bool operator != (const Edge & e) const;
@@ -18,6 +21,10 @@ struct Edge {
 
 Edge::Edge() {}
 Edge::Edge(Node _u, Node _v) : u(_u), v(_v) {}
+
+std::string Edge::output() {
+    return u.Point::output() + " -- " + v.Point::output();
+}
 
 bool Edge::operator == (const Edge & e) const {
     return (u == e.u && v == e.v) || (u == e.v && v == e.u);
