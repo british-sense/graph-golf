55b838281f1ac77ed4710fea9b89e2c709c71665
ko-cha <xtej3g@gmail.com>
2020-02-19 22:21:28 +0900

    :pencil: implemented output function

--- src/node.hpp
+++ src/node.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 #include "point.hpp"
 
 struct Node : Point{
@@ -12,6 +14,9 @@ struct Node : Point{
     Node(int id);
     Node(int h, int w);
 
+    void clear();
+    std::string output();
+
     // operator
     bool operator == (const Node & n) const;
     bool operator != (const Node & n) const;
@@ -19,11 +24,19 @@ struct Node : Point{
 
 Node::Node() {}
 Node::Node(int _id) : id(_id) {}
-Node::Node(int _h, int _w) : Point(_h, _w) {}
+Node::Node(int _h, int _w) : Point(_h, _w), id(_h * _w + _w) {}
+
+// member function
+void Node::clear() {
+    *this = Node();
+}
+std::string Node::output() {
+    return std::to_string(id) + Point::output();
+}
 
 bool Node::operator == (const Node & n) const {
-    return id == n.id;
+    return id == n.id && Point::operator==(n);
 }
 bool Node::operator != (const Node & n) const {
-    return id != n.id;
+    return id != n.id && Point::operator!=(n);
 }
